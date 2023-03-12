bf6838e91f9af366dadf3e7197bb7ce0dc8a2a41
sunwooKim <frod5@kakao.com>
2023-03-12 23:56:04 +0900

    강의 1일차 spring api 실습

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,9 +1,11 @@
 package hello.hellospring.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +20,31 @@ public class HelloController {
     public String helloMvc(@RequestParam String name, Model model) {
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    @GetMapping("/hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam String name, Model model) {
+        return "hello "+name;
+    }
+
+    @GetMapping("/hello-api")
+    @ResponseBody
+    public ResponseEntity<Hello> helloString(@RequestParam String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return ResponseEntity.ok(hello);
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
