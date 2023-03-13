9f31a8c404438ebcc1fa15aaf77c32d549b1811e
sunwooKim <frod5@kakao.com>
2023-03-13 19:04:14 +0900

    강의 2일차 회원 도메인, 레포지토리 생성 실습 및 레포지토리 테스트 코드 작성 실습

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,72 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemoryMemberRepositoryTest {
+    MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+
+    @AfterEach //각 메소드 테스트 끝나고 실행
+    public void afterEach() {
+        memoryMemberRepository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+        memoryMemberRepository.save(member);
+
+        Member result = memoryMemberRepository.findById(member.getId()).get();
+
+        Assertions.assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+
+        memoryMemberRepository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memoryMemberRepository.save(member2);
+
+
+        Member result = memoryMemberRepository.findByName("spring1").get();
+
+        Assertions.assertThat(member1).isEqualTo(result);
+
+
+    }
+
+    @Test
+    public void findAll() {
+
+        Member member1 = new Member();
+        member1.setName("spring1");
+        memoryMemberRepository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memoryMemberRepository.save(member2);
+
+        Member member3 = new Member();
+        member3.setName("spring3");
+        memoryMemberRepository.save(member3);
+
+        Member member4 = new Member();
+        member4.setName("spring4");
+        memoryMemberRepository.save(member4);
+
+        List<Member> resultList = memoryMemberRepository.findByAll();
+
+        Assertions.assertThat(resultList.size()).isEqualTo(4);
+    }
+}
